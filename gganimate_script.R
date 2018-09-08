5c57a1bf6d12addb932d0e078a0ac0695a9fe67e
Gabriel Cabrera <gcabrerag@fen.uchile.cl>
2018-09-08 01:50:06 -0300

    gganimate script added

--- gganimate_script.R
+++ gganimate_script.R
@@ -13,13 +13,16 @@ p_load("tidyverse", "gganimate", "maps", "ggthemes")
 average_wages <- read_delim("dataset/gender-gap-in-average-wages-ilo.csv", delim = ",") %>% 
                  rename(region = Entity, gender_wage = `Gender wage gap (%) (%)`)
 
-world <- ggplot() + borders("world", colour = "gray85", fill = "gray80") + theme_map()
+world <- ggplot() + borders("world", colour = "black", fill = "gray80") + theme_map()
 
-long_lat <- map_data("world")
+world_map <- map_data("world") %>% 
+             left_join(average_wages, by = "region")
 
-world_map <- left_join(average_wages, long_lat, by = "region")
+map <- world + geom_polygon(data = world_map, 
+                            aes(x = long, y = lat, group = group, fill = gender_wage), 
+                            alpha = 0.5) + 
+       scale_fill_gradient(name="gender wage", low = "whitesmoke", high = "red") # darkred       
+# http://www.rpubs.com/knm6/mapsI
 
-map <- world + geom_point( data = world_map, aes(x = long, y = lat, size = gender_wage), colour = 'purple', alpha = .3) +
-               scale_size_continuous(range = c(1, 5), breaks = c(-35, 0, 10, 20, 35)) + labs(size = 'Gender Wage')
 map
 
